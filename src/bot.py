d2dbd2970645ac49c45de8dc39df280ea93b0edd
DmitriiSvarovskii <swarovskidima@gmail.com>
2024-02-04 10:29:46 +0530

    fix

--- src/bot.py
+++ src/bot.py
@@ -5,7 +5,7 @@ import os
 
 from aiogram import Bot, Dispatcher
 
-from .config import BOT_TOKEN
+from src.config import BOT_TOKEN
 from src.handlers import register_user_commands, register_admin_commands
 from src.keyboards import set_main_menu
 
