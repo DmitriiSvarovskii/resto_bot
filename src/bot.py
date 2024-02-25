00e372a0798c51d0ef203d1c6f8803d1777396a4
DmitriiSvarovskii <swarovskidima@gmail.com>
2024-02-25 09:43:42 +0530

    Corrected the error of changing the availability of a dish and a product, made message sending cyclical.

--- src/bot.py
+++ src/bot.py
@@ -2,10 +2,11 @@ import asyncio
 import logging
 import sys
 import os
-from datetime import datetime, timedelta
+
+from apscheduler.triggers.cron import CronTrigger
+from datetime import time
 
 from aiogram import Bot, Dispatcher
-# from aiogram.types import Message
 from aiogram.fsm.storage.redis import RedisStorage, Redis
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
@@ -17,8 +18,10 @@ from utils import set_menu
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), "src.")))
 
-run_time = datetime.now().replace(hour=13,
-                                  minute=0, second=0, microsecond=0)
+
+execution_time = time(hour=13, minute=0)
+
+trigger = CronTrigger(hour=execution_time.hour, minute=execution_time.minute)
 
 logger = logging.getLogger(__name__)
 
@@ -36,11 +39,11 @@ async def main():
     redis = Redis(host=settings.DB_HOST)
     storage = RedisStorage(redis=redis)
     dp: Dispatcher = Dispatcher(storage=storage)
-    schedular = AsyncIOScheduler(timezone='Asia/Kolkata')
-    schedular.add_job(create_mail_group_auto, trigger='date',
-                      run_date=run_time,
-                      kwargs={'bot': bot})
-    schedular.start()
+    scheduler = AsyncIOScheduler(timezone='Asia/Kolkata')
+    scheduler.add_job(create_mail_group_auto,
+                      trigger=trigger, kwargs={'bot': bot})
+
+    scheduler.start()
 
     register_user_commands(dp)
     register_admin_commands(dp)
