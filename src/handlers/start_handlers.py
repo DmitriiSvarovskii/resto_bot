527de51cef4dd728094e56dc5addb43adc9ca143
DmitriiSvarovskii <swarovskidima@gmail.com>
2024-03-08 11:14:53 +0530

    Сделал ограничение на запуск бота только из личных сообщений

--- src/handlers/start_handlers.py
+++ src/handlers/start_handlers.py
@@ -1,6 +1,6 @@
 from aiogram import Router, types
 
-from src.keyboards import main_kb
+from src.keyboards import main_kb, admin_kb
 from src.lexicons import LEXICON_RU
 from src.db import customer_db
 from src.utils import customer_utils
@@ -12,16 +12,24 @@ router = Router(name=__name__)
 
 @router.message(CommandStart())
 async def process_start_command(message: types.Message):
-    customer_data = await customer_utils.create_customer_data_from_message(
-        message=message
-    )
-    await customer_db.add_new_user_to_database(
-        customer_data=customer_data
-    )
+    if message.chat.type == 'private':
+        customer_data = await customer_utils.create_customer_data_from_message(
+            message=message
+        )
+        await customer_db.add_new_user_to_database(
+            customer_data=customer_data
+        )
 
-    keyboard = await main_kb.create_kb_main(message.chat.id)
+        keyboard = await main_kb.create_kb_main(message.chat.id)
 
-    await message.answer(
-        text=LEXICON_RU['start'],
-        reply_markup=keyboard
-    )
+        await message.answer(
+            text=LEXICON_RU['start'],
+            reply_markup=keyboard
+        )
+    else:
+        await message.reply(
+            text='Извините, этот бот предназначен для использования только в '
+            'личных чатах. Если вы хотите сделать заказ, '
+            'пожалуйста, перейдите в сам бот',
+            reply_markup=admin_kb.create_kb_sale_group()
+        )
