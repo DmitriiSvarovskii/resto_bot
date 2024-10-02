e8a6ed093934b84df4bdba5a1f31f3cfe10c2ad1
bigdigital <tech@bigdigital.com>
2024-10-02 13:19:54 +0700

    добавил обработку магазина (если он не работает, то в него не зайти)

--- src/handlers/customer_handlers/select_store.py
+++ src/handlers/customer_handlers/select_store.py
@@ -1,7 +1,7 @@
 from aiogram import Router, types, F
 
 from src.keyboards import main_kb
-from src.db import store_db
+from src.db import store_db, customer_db
 from src.lexicons import text_main_menu_en, text_main_menu_ru
 from src.callbacks import StoreCbDataList
 
@@ -14,21 +14,32 @@ async def press_select_store(
     callback: types.CallbackQuery,
     callback_data: StoreCbDataList
 ):
-    if callback.from_user.language_code == 'ru':
-        text_main_menu = text_main_menu_ru
+    store_info = await store_db.get_store_info(store_id=callback_data.store_id)
+    user_info = await customer_db.get_user_info_by_id(
+        user_id=callback.message.chat.id
+    )
+
+    if store_info.is_active or user_info.admin:
+        if callback.from_user.language_code == 'ru':
+            text_main_menu = text_main_menu_ru
+        else:
+            text_main_menu = text_main_menu_en
+
+        keyboard = await main_kb.create_kb_main(
+            language=callback.from_user.language_code,
+            user_id=callback.message.chat.id,
+            store_id=callback_data.store_id
+        )
+
+        await callback.message.edit_text(
+            text=text_main_menu.main_menu_dict['start'],
+            reply_markup=keyboard
+        )
     else:
-        text_main_menu = text_main_menu_en
-
-    keyboard = await main_kb.create_kb_main(
-        language=callback.from_user.language_code,
-        user_id=callback.message.chat.id,
-        store_id=callback_data.store_id
-    )
-
-    await callback.message.edit_text(
-        text=text_main_menu.main_menu_dict['start'],
-        reply_markup=keyboard
-    )
+        await callback.answer(
+            text="Извините, магазин временно не работает",
+            show_alert=True
+        )
 
 
 @router.callback_query(StoreCbDataList.filter(F.type_press == 'view-list'))
